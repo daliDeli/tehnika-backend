34b0d25df045fdd2abf9576d7b105ff43690e7de
daliDeli <bruns8900@gmail.com>
2018-03-02 21:18:51 +0100

    trying to fix error with lock files

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+//https://mysterious-crag-26867.herokuapp.com/ | https://git.heroku.com/mysterious-crag-26867.git
+
 var express = require("express"),
- path = require('path'),
+path = require('path'),
 formidable = require('formidable'),
 fs = require('fs'),
 multer = require('multer'),
@@ -13,12 +15,12 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var Storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, "../frontend/public/assets/img/upload");
-    },
-    filename: function (req, file, callback) {
-        callback(null, Date.now() + file.originalname);
-    }
+   destination: function (req, file, callback) {
+       callback(null, "../frontend/public/assets/img/upload");
+   },
+   filename: function (req, file, callback) {
+       callback(null, Date.now() + file.originalname);
+   }
 });
 var upload = multer({ storage: Storage }).array("files", 3);
 
@@ -30,11 +32,11 @@ var upload = multer({ storage: Storage }).array("files", 3);
 // });
 
 var db = mysql.createConnection({
-    host: "localhost",
-    user: "dalibor",
-    password: "dado",
-    database: "tehnikatestdb"
-  });
+   host: "localhost",
+   user: "dalibor",
+   password: "dado",
+   database: "tehnikatestdb"
+ });
 
 // var db = mysql.createConnection({
 //     host: "localhost",
@@ -45,147 +47,147 @@ var db = mysql.createConnection({
 
 // headers- omogucava CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+   res.header(
+       "Access-Control-Allow-Headers",
+       "Origin, X-Requested-With, Content-Type, Accept"
+   );
+   next();
 });
 
 // get metode 
 
 /*
-    forma za testiranje
+   forma za testiranje
 */
 app.get('/upload', (req, res)=> {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="files"><br>');
-    res.write('<input type="text" name="id"><br>');    
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+   res.writeHead(200, {'Content-Type': 'text/html'});
+   res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+   res.write('<input type="file" name="files"><br>');
+   res.write('<input type="text" name="id"><br>');    
+   res.write('<input type="submit">');
+   res.write('</form>');
+   return res.end();
 });
 // vraca sve kategorije ako ne prosledis parametar 
 // parametri: category => id kategorije
 app.get("/api/products",(req,res)=>{
-    // console.log(req.query.category);
-    // return res.end(req.query);
-    var queryString = '';
-    if(req.query.category == undefined){
-        queryString = "SELECT * FROM tehnikaall";
-    }else{
-        queryString = `SELECT * FROM tehnikaAll where kategorije_id = ${req.query.category}`   ;        
-    }
-    // return res.end(queryString);
-    db.query(queryString, (error, rows, fields)=>{
-        if(error){
-            res.status(400);
-            return res.send(error.message);
-        }else{           
-            return res.send(JSON.stringify(rows));
-        }
-    });   
-    //return res.end(req.query.category);
+   // console.log(req.query.category);
+   // return res.end(req.query);
+   var queryString = '';
+   if(req.query.category == undefined){
+       queryString = "SELECT * FROM tehnikaall";
+   }else{
+       queryString = `SELECT * FROM tehnikaAll where kategorije_id = ${req.query.category}`   ;        
+   }
+   // return res.end(queryString);
+   db.query(queryString, (error, rows, fields)=>{
+       if(error){
+           res.status(400);
+           return res.send(error.message);
+       }else{           
+           return res.send(JSON.stringify(rows));
+       }
+   });   
+   //return res.end(req.query.category);
 });
 // vraca sve kategorije
 app.get('/api/category', (req, res)=> {
-    db.query("select * from kategorije", (error, rows, fields) => {
-        if(!error){                        
-            res.send(rows);
-            return res.end();
-        }
-        res.status(400);
-        return res.end(error.message);
-    });
+   db.query("select * from kategorije", (error, rows, fields) => {
+       if(!error){                        
+           res.send(rows);
+           return res.end();
+       }
+       res.status(400);
+       return res.end(error.message);
+   });
 });
 // vraca proizvod sa odredjenim id-em
 app.get('/api/product/single', (req, res) => {
-    db.query("SELECT * FROM tehnikaAll where id = " + req.query.id + " ", (error, rows, fileds) => {
-        res.send(rows[0]);
-    });
+   db.query("SELECT * FROM tehnikaAll where id = " + req.query.id + " ", (error, rows, fileds) => {
+       res.send(rows[0]);
+   });
 });
 // post medote
 
 
 //kreira proizvod
 app.post('/api/product/create', (req, res) => {
-    console.log(`${req.body.files}`);
-    // return res.end();    
-    db.query(`insert into proizvodi (naziv, opis, cijena, kategorije_id, slug, slika) 
-    values ('${req.body.naziv}' , '${req.body.opis}' , '${req.body.cijena}' , ${req.body.kategorija},
-     '${slug(req.body.naziv)}', '` + req.body.files + `')`,
+   console.log(`${req.body.files}`);
+   // return res.end();    
+   db.query(`insert into proizvodi (naziv, opis, cijena, kategorije_id, slug, slika) 
+   values ('${req.body.naziv}' , '${req.body.opis}' , '${req.body.cijena}' , ${req.body.kategorija},
+    '${slug(req.body.naziv)}', '` + req.body.files + `')`,
 (error, rows, fields) => {
-    if(error){
-        res.status(400);
-        console.log(error.message);            
-        return res.end("error");
-    }   
-    return res.end("success");
+   if(error){
+       res.status(400);
+       console.log(error.message);            
+       return res.end("error");
+   }   
+   return res.end("success");
 });
 });
 // uploduje fajl u folder images (ako nema napraviti rucno) i upipsuje u bazu
 // potrebno proslijediti i id artikla na koji se slika odnosi
 // preporucujem da pogledas dropzone.js
 app.post('/api/fileupload', (req, res) => {
-    var path = '';
-    upload(req, res, function (err) {      
-        return res.send(req.files);
-    });
+   var path = '';
+   upload(req, res, function (err) {      
+       return res.send(req.files);
+   });
 });
 
 // kreira kategoriju
 app.post('/api/cateogroy/create', (req, res) => {
-    db.query(`insert into kategorije (naziv, slug) values ('${req.body.naziv}', '${slug(req.body.naziv)}')`,
-    (error, rows, fields) => {
-        if(error){
-            res.status(400);
-            return res.end(error.message);
-        }
-        return res.end("success");
-    });
+   db.query(`insert into kategorije (naziv, slug) values ('${req.body.naziv}', '${slug(req.body.naziv)}')`,
+   (error, rows, fields) => {
+       if(error){
+           res.status(400);
+           return res.end(error.message);
+       }
+       return res.end("success");
+   });
 });
 
 // delete
 // kategorije
 app.delete('/api/category/delete/:categoryID', (req, res) => {
-    // return res.end(req.body.id);??
-    db.query(`delete from kategorije where id = ${req.params.categoryID}`, (error, rows, fields) => {
-        if(error){
-            res.status(400);
-            return res.end(error.message);
-        }
-        return res.end('success');
-    });
+   // return res.end(req.body.id);??
+   db.query(`delete from kategorije where id = ${req.params.categoryID}`, (error, rows, fields) => {
+       if(error){
+           res.status(400);
+           return res.end(error.message);
+       }
+       return res.end('success');
+   });
 });
 
 // proizvodi
 app.post('/api/products/delete', (req, res) => {
-    console.log(req.body.productID);
-    db.query(`delete from proizvodi where id= ${req.body.productID}`, (error, rows, fields) => {
-        if(error){
-            res.status(400);
-            return res.end(error.message);
-        }
+   console.log(req.body.productID);
+   db.query(`delete from proizvodi where id= ${req.body.productID}`, (error, rows, fields) => {
+       if(error){
+           res.status(400);
+           return res.end(error.message);
+       }
 
-        return res.end('success');
-    });
+       return res.end('success');
+   });
 });
 // brisanje svih proizvoda i kategorije na osnovu id kategorije
 app.delete('api/categories/with-products/delete/:categoryID', (req, res) => {
-    db.query(`delete from proizvodi where kategorije_id = ${req.params.categoryID}`, (error, rows, fields) => {
-        if(error) {
-            return res.end(error.body);
-        }
-        db.query(`delete from kategorije where id = ${req.params.categoryID}`, (error, rows, fields) => {
-            if(error){
-                res.status(400);
-                return res.end(error.message);
-            }
-            return res.end('success');
-        });
-    });
+   db.query(`delete from proizvodi where kategorije_id = ${req.params.categoryID}`, (error, rows, fields) => {
+       if(error) {
+           return res.end(error.body);
+       }
+       db.query(`delete from kategorije where id = ${req.params.categoryID}`, (error, rows, fields) => {
+           if(error){
+               res.status(400);
+               return res.end(error.message);
+           }
+           return res.end('success');
+       });
+   });
 });
 
 // update
@@ -193,27 +195,27 @@ app.delete('api/categories/with-products/delete/:categoryID', (req, res) => {
 //proizvoda 
 app.put('/api/products/update', (req, res) => {
 
-   db.query(`update proizvodi set naziv = '${req.body.naziv}', opis = '${req.body.opis}', cijena = '${req.body.cijena}',
-    kategorije_id = ${req.body.kategorija}, slug = '${slug(req.body.naziv)}' where id = ${req.body.id}`,
-    (error, rows, fields) => {
-        if(error) {
-            res.status(400);
-            return res.end(error.message);
-        }
-        return res.end('success');
-    }); 
+  db.query(`update proizvodi set naziv = '${req.body.naziv}', opis = '${req.body.opis}', cijena = '${req.body.cijena}',
+   kategorije_id = ${req.body.kategorija}, slug = '${slug(req.body.naziv)}' where id = ${req.body.id}`,
+   (error, rows, fields) => {
+       if(error) {
+           res.status(400);
+           return res.end(error.message);
+       }
+       return res.end('success');
+   }); 
 }); 
 
 // kategorija 
 app.put('/api/category/update', (req, res) => {
-    db.query(`update kategorije set naziv = '${req.body.naziv}', slug = '${slug(req.body.naziv)}' where id = ${req.body.naziv}`,
-        (error, rows, fields)=>{
-            if(error){
-                res.status(400);
-                return res.end(error.message);
-            }
-            return res.end('success');
-        });
+   db.query(`update kategorije set naziv = '${req.body.naziv}', slug = '${slug(req.body.naziv)}' where id = ${req.body.naziv}`,
+       (error, rows, fields)=>{
+           if(error){
+               res.status(400);
+               return res.end(error.message);
+           }
+           return res.end('success');
+       });
 });
 
 // var server = app.listen(3300, listening);
